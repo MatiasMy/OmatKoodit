d96d7201eef17103f5dd609d3fda97162114493f
myllmat22 <matiasmyllymaki05@gmail.com>
2023-06-06 12:18:53 +0300

    helico

---
+++ helico/index.php
@@ -0,0 +1,18 @@
+<?php require "head.html"; ?>
+
+<img src="img/" alt="game view" id="gamview">
+
+<div class="buttons">
+    <div class="button1">
+        <button></button>
+    </div>
+    <div class="button2">
+        <button></button>
+    </div>
+    <div class="button3">
+        <button></button>
+    </div>
+    <div class="button4">
+        <button></button>
+    </div>
+</div>
