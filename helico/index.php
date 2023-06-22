fa676c0b9114fc59119f05d6428d468465ed8184
myllmat22 <matiasmyllymaki05@gmail.com>
2023-06-22 18:29:17 +0300

    helico

--- helico/index.php
+++ helico/index.php
@@ -3,12 +3,12 @@
 <img src="img/skull.jpg" alt="game view" id="gamview" class="kuva">
 
 <div class="puhe1">
-<p calss="puhe">agf</p>
+<p calss="puhe" onclick="teksti()">paina minua</p>
 </div>
 
 <div name="napit">
     <div name="n1">
-        <button>asfasfasf</button>
+        <button></button>
     </div>
     <div name="n2">
         <button></button>
