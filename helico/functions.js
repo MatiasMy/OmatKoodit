73e3abe7d9f28dbb2bfb5e3aa111711b87bc373c
myllmat22 <matiasmyllymaki05@gmail.com>
2023-06-23 23:11:51 +0300

    helico

---
+++ helico/functions.js
@@ -0,0 +1,6 @@
+function teksti(){
+    document.getElementById("kuva").src="skull.jpg"
+}
+function refreshPage(){
+    window.location.reload();
+} 
--- helico/start.up.php
+++
@@ -1,5 +0,0 @@
-<?php require "head.html"; ?>
-
-<div name="nappula">
-    <a href="index.php">start</a>
-</div>
