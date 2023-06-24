bd0b72b7a29fbed8739fdb90bd708f1282955c20
myllmat22 <matiasmyllymaki05@gmail.com>
2023-06-24 03:07:08 +0300

    helico

--- helico/functions.js
+++ helico/functions.js
@@ -1,5 +1,14 @@
-function teksti(){
-    document.getElementById("kuva").src="skull.jpg"
+function a1(){
+    document.getElementById("puhe").innerHTML="thats strange"
+}
+function a2(){
+    document.getElementById("puhe").innerHTML="hello james"
+}
+function a3(){
+    document.getElementById("puhe").innerHTML="you are not"
+}
+function a4(){
+    document.getElementById("puhe").innerHTML="who are you?"
 }
 function refreshPage(){
     window.location.reload();
